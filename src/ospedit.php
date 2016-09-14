1741b3c3edefd96e4b6baad84a1fdac83bc1cd92
spasutto <sylvainpasutto@gmail.com>
2016-09-14 13:02:34 +0200

    save bug

--- src/ospedit.php
+++ src/ospedit.php
@@ -83,7 +83,16 @@ if ($operation=="load")
 	if (!file_exists($file) || !is_file($file))
 		echo json_encode(["status"=>"ko", "message"=> "\"".$file."\""." isn't a file"]);
 	else
-		echo json_encode(["status"=>"ok", "message"=> "", "content"=> utf8_encode($content)]);
+	{
+		$response = json_encode(["status"=>"ok", "message"=> "", "content"=> $content]);
+		if (json_last_error() != JSON_ERROR_NONE)
+		{
+			$response = json_encode(["status"=>"ok", "message"=> "", "content"=> utf8_encode($content)]);
+			if (json_last_error() != JSON_ERROR_NONE)
+				$response = json_encode(["status"=>"ko", "message"=> "", "message"=> "unable to encode\"".$file."\" : ".json_last_error_msg()]);
+		}
+		echo $response;
+	}
 	exit(0);
 }
 else if ($operation=="save")
@@ -95,10 +104,38 @@ else if ($operation=="save")
 		$content = "";
 	else
 		$content = $_POST['content'];
-	if (@file_put_contents($file, htmlspecialchars_decode($content)))
+	$fenc = mb_detect_encoding($content);
+	/*echo $fenc."\r\n";
+	echo $content."\r\n";*/
+	$content = iconv($fenc, 'utf-8', $content);
+	/*echo $content."\r\n";*/
+	//$content = utf8_encode($content);
+	//echo $content."\r\n";
+	if (@file_put_contents($file, $content))
 		echo json_encode(["status"=>"ok", "message"=> "file saved."]);
 	else
 		echo json_encode(["status"=>"ko", "message"=> "unable to save file."]);
+
+
+	/*$fc = file_get_contents($file);
+	$fenc = mb_detect_encoding($fc);
+	echo $fenc."\r\n";
+	echo $content."\r\n";
+	if (strtolower($fenc) != 'utf-8')
+		$fc = iconv($fenc, 'utf-8', file_get_contents($file));
+	echo $content."\r\n";
+	$handle = fopen("php://memory", "rw");
+	fwrite($handle, $fc);
+	fseek($handle, 0);
+	$content = fread($handle,filesize($file));
+	// remove BOM https://fr.wikipedia.org/wiki/Indicateur_d%27ordre_des_octets
+	if (substr($content, 0, 3) == chr(0xef).chr(0xbb).chr(0xbf))
+		$content = substr($content, 3);
+	else if (substr($content, 0, 2) == chr(0xfe).chr(0xff))
+		$content = substr($content, 2);
+	echo $content."\r\n";
+echo "pouet.";*/
+
 	exit(0);
 }
 else if ($operation=="checkfile")
